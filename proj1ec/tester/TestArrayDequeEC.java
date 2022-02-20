6bb3a1708482bbe20e9fca518db5d2c7f92c57f5
HanxiChen <71202369+HanxiChen@users.noreply.github.com>
2022-02-21 00:10:02 +0800

    proj1 ec

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -10,51 +10,50 @@ import java.util.Random;
 import java.util.StringJoiner;
 
 public class TestArrayDequeEC {
-    ArrayDequeSolution<Integer> solution = new ArrayDequeSolution<>();
-    StudentArrayDeque<Integer> student = new StudentArrayDeque<>();
-    Random random = new Random();
-    StringJoiner stackTrace = new StringJoiner("");
-    private final int iterations = 100000;
-
     @Test
-    public void randomlyCompareStudentArrayDequeToSolution() {
-        double variate;
-        int key;
-        int count = 0;
-        Integer exptected = 0;
+    public void test1() {
+        StudentArrayDeque<Integer> student = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> ad = new ArrayDequeSolution<>();
+
+        Integer expected = 0;
         Integer actual = 0;
-        stackTrace.add("\n"); // Ensures first invocation is on a new line.
-        while (count < iterations) {
-            variate = random.nextDouble();
-            key = random.nextInt(100);
-            count++;
-            if (variate < 0.25)  {
-                if (student.size() == 0) {
-                    continue;
-                }
-                actual = student.removeLast();
-                exptected = solution.removeLast();
-                stackTrace.add("removeLast()\n");
-                assertEquals(stackTrace.toString(), exptected, actual);
-            } else if (0.26 <= variate && variate < 0.5) {
-                solution.addFirst(key);
-                student.addFirst(key);
-                stackTrace.add(String.format("addFirst(%d)\n", key));
-                assertEquals(stackTrace.toString(), solution.size(), student.size());
-            } else if (0.5 <= variate && variate < 0.75) {
+        StringJoiner sj = new StringJoiner("");
+
+        int N = 5000;
+        for (int i = 0; i < N; i++) {
+            int operationNumber = StdRandom.uniform(0, 4);
+            int randVal = StdRandom.uniform(0, 100);
+
+            if (operationNumber == 0) {
+                student.addFirst(randVal);
+                ad.addFirst(randVal);
+                sj.add("\naddFirst(" + randVal + ")");
+                assertEquals(sj.toString(), ad.size(), student.size());
+            } else if (operationNumber == 1) {
+                student.addLast(randVal);
+                ad.addLast(randVal);
+                sj.add("\naddLast(" + randVal + ")");
+                assertEquals(sj.toString(), ad.size(), student.size());
+            } else if (operationNumber == 2) {
                 if (student.size() == 0) {
                     continue;
                 }
                 actual = student.removeFirst();
-                exptected = solution.removeFirst();
-                stackTrace.add("removeFirst()\n");
-                assertEquals(stackTrace.toString(), exptected, actual);
+                expected = ad.removeFirst();
+                sj.add("\nremoveFirst()");
+                assertEquals(sj.toString(), expected, actual);
             } else {
-                solution.addLast(key);
-                student.addLast(key);
-                stackTrace.add(String.format("addLast(%d)\n", key));
-                assertEquals(stackTrace.toString(), solution.size(), student.size());
+                if (student.size() == 0) {
+                    continue;
+                }
+                actual = student.removeLast();
+                expected = ad.removeLast();
+                sj.add("\nremoveLast()");
+                assertEquals(sj.toString(), expected, actual);
             }
         }
     }
+
+
+
 }
